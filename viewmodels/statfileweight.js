e9ea9347e326f5d2b5c8340c530840f6961a071f
Dmytro Nechepurenko <d.nechepurenko@astoundcommerce.com>
2018-03-04 19:31:43 +0200

    WIP: imports graph

--- viewmodels/statfileweight.js
+++ viewmodels/statfileweight.js
@@ -1,73 +1,37 @@
 'use strict';
 
-// const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
-// const d3fmt = require('d3-format');
-// const d3scale = require('d3-scale');
-//
-// function prepareComponentsFileSizesStat(guideSrc) {
-//     let sizeStats = [];
-//
-//     function getfileSize(path) {
-//         const fileString = fs.readFileSync(path, 'utf8');
-//         const stripedFile = fileString.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '$1');
-//
-//         return Buffer.byteLength(stripedFile, 'utf8');
-//     }
-//
-//     function findComponents(url) {
-//         const dir = fs.readdirSync(url);
-//
-//         for (let i = 0; i < dir.length; i++) {
-//             const name = dir[i];
-//             const target = path.join(url, name);
-//             const stats = fs.statSync(target);
-//
-//             if (stats.isFile()) {
-//                 if (path.extname(name) === '.scss') {
-//                     const fileSize = getfileSize(target);
-//                     sizeStats.push({
-//                         filename: path.basename(name),
-//                         size: fileSize,
-//                         view: {
-//                             size: d3fmt.format('.2s')(fileSize) + 'B'
-//                         },
-//                         svg: {
-//                             radius: '',
-//                             width: ''
-//                         }
-//                     });
-//                 }
-//             } else if (stats.isDirectory()) {
-//                 findComponents(target);
-//             }
-//         }
-//     }
-//
-//     findComponents(guideSrc);
-//
-//     sizeStats.sort((a, b) => b.size - a.size);
-//
-//     const max = sizeStats[0].size;
-//     const scale = d3scale.scaleLinear()
-//         .domain([0, max])
-//         .range([0, 400]);
-//
-//     sizeStats.forEach((item) => {
-//         const circleSize = scale(item.size);
-//         item.svg = {
-//             radius: circleSize / 2,
-//             size: circleSize
-//         };
-//     });
-//
-//     return sizeStats;
-// }
+// const _uniq = require('lodash.uniq');
 
-function getImports(importsGraph) {
-    const projectName = 'test';
+function getfileSize(string) {
+    return Buffer.byteLength(string, 'utf8');
+}
+
+function getfileSizeWithoutComments(path) {
+    const fileString = fs.readFileSync(path, 'utf8');
+    const stripedFile = fileString.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '$1');
+
+    return getfileSize(stripedFile);
+}
+
+function getResultedFileSize(name, pathToCSS) {
+    const filePath = path.join(pathToCSS, name.replace(/\.scss/, '.css'));
+    let fileString = '';
+
+    if (fs.existsSync(filePath)) {
+        fileString = fs.readFileSync(filePath, 'utf8');
+    }
+
+    return getfileSize(fileString);
+}
+
+function getImports(importsGraph, projectName, pathToCSS) {
     const pathToSCSS = new RegExp(path.join(importsGraph.dir, '/'));
-    let importsPaths = [projectName];
+    let importsPaths = [{
+        id: projectName,
+        size: 0
+    }];
 
     for (let prop in importsGraph.index) {
         if (!importsGraph.index.hasOwnProperty(prop)) {
@@ -77,30 +41,39 @@ function getImports(importsGraph) {
         const fileName = path.basename(pathStr);
         const isPartial = /^_/i.test(fileName);
 
-        if (isPartial) {
-            const importedBy = importsGraph.index[prop].importedBy;
-            const dest = pathStr.replace(pathToSCSS, '').replace(new RegExp(fileName), '').split('/');
-            // get partial size
-
-            importedBy.forEach(importedBy => {
-                // push standalone file (root)
-                const importFile = importedBy.toString().replace(pathToSCSS, '');
-                let cummulativePath = projectName + '/' + importFile;
-                importsPaths.push(cummulativePath);
-
-                // push missing folders
-                for (let i = 0; i < dest.length - 1; i++) {
-                    cummulativePath = cummulativePath + '/' + dest[i];
-                    importsPaths.push(cummulativePath);
-                }
-
-                // push file
-                importsPaths.push(cummulativePath + '/' + fileName);
-            });
+        if (!isPartial) {
+            continue;
         }
+        const importedBy = importsGraph.index[prop].importedBy;
+        const dest = pathStr.replace(pathToSCSS, '').replace(new RegExp(fileName), '').split('/');
+
+        importedBy.forEach(importedBy => {
+            // push standalone file (root)
+            const importFile = importedBy.toString().replace(pathToSCSS, '');
+            let cummulativePath = projectName + '/' + importFile;
+            importsPaths.push({
+                id: cummulativePath,
+                size: getResultedFileSize(importFile, pathToCSS)
+            });
+
+            // push missing folders
+            for (let i = 0; i < dest.length - 1; i++) {
+                cummulativePath = cummulativePath + '/' + dest[i];
+                importsPaths.push({
+                    id: cummulativePath,
+                    size: 0
+                });
+            }
+
+            // push file
+            importsPaths.push({
+                id: cummulativePath + '/' + fileName,
+                size: getfileSizeWithoutComments(prop)
+            });
+        });
     }
 
-    // console.log( JSON.stringify(_uniq(importsPaths.sort())) )
+    // console.log( JSON.stringify(importsPaths, null, '\t'))
 
     // return JSON.stringify(_uniq(importsPaths.sort()));
 }
