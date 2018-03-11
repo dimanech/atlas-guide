184a23565cdb4678da5b59c792e759eff66d7e31
Dmytro Nechepurenko <d.nechepurenko@astoundcommerce.com>
2018-03-11 14:59:54 +0200

    ADD: selectors statistic

--- viewmodels/statproject.js
+++ viewmodels/statproject.js
@@ -398,6 +398,69 @@ function selectorsListTops(array) {
     return longestSelectors;
 }
 
+function selectors(selectors) {
+    let universalSelectors = [];
+    let jsPrefixedSelectors = [];
+    let idSelectors = [];
+    let attributeSelectors = 0;
+
+    selectors.values.forEach(selector => {
+        if (/^\*|\*$/.test(selector)) {
+            universalSelectors.push(selector);
+        }
+        if (/js-/.test(selector)) {
+            jsPrefixedSelectors.push(selector);
+        }
+        if (/#/.test(selector)) {
+            idSelectors.push(selector);
+        }
+        if (/\[/.test(selector)) {
+            attributeSelectors++;
+        }
+    });
+
+    const selectorsStat = [{
+        'name': 'Id',
+        'count': selectors.id
+    }, {
+        'name': 'Class',
+        'count': selectors['class']
+    }, {
+        'name': 'Pseudo class',
+        'count': selectors.pseudoClass
+    }, {
+        'name': 'Pseudo element',
+        'count': selectors.pseudoElement
+    }, {
+        'name': 'Type',
+        'count': selectors.type
+    }, {
+        'name': 'Attribute',
+        'count': attributeSelectors
+    }, {
+        'name': 'Universal',
+        'count': universalSelectors.length
+    }, {
+        'name': 'js- prefixed',
+        'count': jsPrefixedSelectors.length
+    }];
+
+    return {
+        stat: selectorsStat.sort((a, b) => b.count - a.count),
+        jsPrefixedSelectors: jsPrefixedSelectors,
+        universalSelectors: universalSelectors,
+        idSelectors: idSelectors
+    };
+}
+
+function importantRules(rules) {
+    const isHaveRules = rules !== undefined;
+    return {
+        'count': isHaveRules ? rules.length : 0,
+        'rules': isHaveRules ? rules : []
+    };
+}
+
 function statProject(stat) {
     let projectCssStat = [];
 
@@ -417,7 +480,9 @@ function statProject(stat) {
             specificityTops: specificityTops(stats.selectors.getSpecificityValues()),
             rulesizeChart: ruleSizeChart(stats),
             rulesizeTops: ruleSizeStat(stats.rules.selectorRuleSizes),
-            selectorsListTops: selectorsListTops(stats.selectors.selectorsLists)
+            selectorsListTops: selectorsListTops(stats.selectors.selectorsLists),
+            selectors: selectors(stats.selectors),
+            importantRules: importantRules(stats.declarations.important)
         });
     });
 
