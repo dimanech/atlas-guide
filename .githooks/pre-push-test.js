7506ab6ead28996817f4981b44b816643e56d7d5
Dmytro Nechepurenko <d.nechepurenko@astoundcommerce.com>
2018-03-26 02:02:51 +0300

    UP: git hooks to show process message

--- .githooks/pre-push-test.js
+++ .githooks/pre-push-test.js
@@ -5,6 +5,7 @@ const { exec } = require('child_process');
 const command = 'npm test';
 
 exec(command, (error, stdout, stderr) => {
+    console.log('[GUARD]: Run tests...');
     if (error) {
         console.error(stdout);
         console.log('[GUARD]: Tests not passed');
