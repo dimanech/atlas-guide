c6a0fe428481995295020f663ba7291e2dd35776
Dmytro Nechepurenko <dimanechepurenko@gmail.com>
2019-01-17 15:51:08 +0200

    UP: remove unneeded file

--- models/config/templates.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const fillTemplatesConfig = require('./utils').fillTemplatesConfig;
-
-function getTemplates(config) {
-    return fillTemplatesConfig(config.templates, '../../views/templates/', 'template');
-}
-
-module.exports = getTemplates;
