973e4253bbaaabbee081374c46af301333ed00f2
Dmytro Nechepurenko <dimanechepurenko@gmail.com>
2019-02-11 17:53:19 +0200

    UP: CSS font string parser

--- models/componentstat/utils/cssfontparser.js
+++ models/componentstat/utils/cssfontparser.js
@@ -1,81 +1,149 @@
 'use strict';
 
 /*
- Copied from https://github.com/bramstein/css-font-parser
+ Based on https://github.com/bramstein/css-font-parser
  */
 
 /**
  * @enum {number}
  */
-const states = {
+var states = {
     VARIATION: 1,
     LINE_HEIGHT: 2,
-    FONT_FAMILY: 3
+    FONT_FAMILY: 3,
+    BEFORE_FONT_FAMILY: 4
 };
+
+function isFontSize(buffer) {
+    return (/^((xx|x)-large|(xx|s)-small|small|large|medium)$/.test(buffer) ||
+        /^(larg|small)er$/.test(buffer) ||
+        /^([+-])?([0-9]*\.)?[0-9]+(em|ex|ch|rem|vh|vw|vmin|vmax|px|mm|cm|in|pt|pc|%)$/.test(buffer));
+}
+
+function isLineHeight(buffer) {
+    return /^([+-])?([0-9]*\.)?[0-9]+(em|ex|ch|rem|vh|vw|vmin|vmax|px|mm|cm|in|pt|pc|%)?$/.test(buffer);
+}
+
+/**
+ * Attempt to parse a string as an identifier. Return
+ * a normalized identifier, or null when the string
+ * contains an invalid identifier.
+ *
+ * @param {string} str
+ * @return {string|null}
+ */
+function parseIdentifier(str) {
+    var identifiers = str.replace(/^\s+|\s+$/, '').replace(/\s+/g, ' ').split(' ');
+
+    for (var i = 0; i < identifiers.length; i += 1) {
+        if (/^(-?\d|--)/.test(identifiers[i]) ||
+            !/^([_a-zA-Z0-9-]|[^\0-\237]|(\\[0-9a-f]{1,6}(\r\n|[ \n\r\t\f])?|\\[^\n\r\f0-9a-f]))+$/
+                .test(identifiers[i])) {
+            return null;
+        }
+    }
+    return identifiers.join(' ');
+}
+
+function parseVariation(buffer, result) {
+    switch (true) {
+        case isFontSize(buffer):
+            result['font-size'] = buffer;
+            break;
+        case /^(italic|oblique)$/.test(buffer):
+            result['font-style'] = buffer;
+            break;
+        case /^small-caps$/.test(buffer):
+            result['font-variant'] = buffer;
+            break;
+        case /^(bold(er)?|lighter|[1-9]00)$/.test(buffer):
+            result['font-weight'] = buffer;
+            break;
+        case /^((ultra|extra|semi)-)?(condensed|expanded)$/.test(buffer):
+            result['font-stretch'] = buffer;
+            break;
+    }
+}
+
+function getQuotedString(input, i, currentChar) {
+    var startedQuote = currentChar;
+    var closedQuote = i + 1;
+
+    do {
+        closedQuote = input.indexOf(startedQuote, closedQuote) + 1;
+        if (!closedQuote) {
+            // If a string is not closed by a ' or " return null.
+            return null;
+        }
+    } while (input.charAt(closedQuote - 2) === '\\');
+
+    return {
+        string: input.slice(i, closedQuote),
+        endPosition: closedQuote
+    };
+}
 
 /**
  * @param {string} input
- * @return {Object}
+ * @return {Object|null}
  */
 function parse(input) {
-    let state = states.VARIATION,
+    var state = states.VARIATION,
         buffer = '',
         result = {
             'font-family': []
         };
 
-    for (let c, i = 0; c = input.charAt(i); i += 1) { // eslint-disable-line
-        if (state === states.FONT_FAMILY && (c === '"' || c === '\'')) {
-            let index = i + 1;
+    for (var i = 0; i < input.length; i += 1) {
+        var currentChar = input.charAt(i);
 
-            // consume the entire string
-            do {
-                index = input.indexOf(c, index) + 1;
-                if (!index) { // eslint-disable-line
-                    // If a string is not closed by a ' or " return null.
-                    // TODO: Check to see if this is correct.
-                    return null;
-                }
-            } while (input.charAt(index - 2) === '\\');
-
-            result['font-family'].push(input.slice(i + 1, index - 1).replace(/\\('|")/g, '$1'));
-
-            i = index - 1;
-            buffer = '';
-        } else if (state === states.FONT_FAMILY && c === ',') {
-            if (!/^\s*$/.test(buffer)) {
-                result['font-family'].push(buffer.replace(/^\s+|\s+$/, '').replace(/\s+/g, ' '));
-                buffer = '';
+        if (state === states.BEFORE_FONT_FAMILY && (currentChar === '"' || currentChar === '\'')) {
+            var quotedStr = getQuotedString(input, i, currentChar);
+            if (quotedStr === null) {
+                return null;
             }
-        } else if (state === states.VARIATION && (c === ' ' || c === '/')) {
-            if (/^((xx|x)-large|(xx|s)-small|small|large|medium)$/.test(buffer) ||
-                /^(larg|small)er$/.test(buffer) ||
-                /^(\+|-)?([0-9]*\.)?[0-9]+(em|ex|ch|rem|vh|vw|vmin|vmax|px|mm|cm|in|pt|pc|%)$/.test(buffer)) {
-                state = c === '/' ? states.LINE_HEIGHT : states.FONT_FAMILY;
-                result['font-size'] = buffer;
-            } else if (/^(italic|oblique)$/.test(buffer)) {
-                result['font-style'] = buffer;
-            } else if (/^small-caps$/.test(buffer)) {
-                result['font-variant'] = buffer;
-            } else if (/^(bold(er)?|lighter|normal|[1-9]00)$/.test(buffer)) {
-                result['font-weight'] = buffer;
-            } else if (/^((ultra|extra|semi)-)?(condensed|expanded)$/.test(buffer)) {
-                result['font-stretch'] = buffer;
-            }
-            buffer = '';
-        } else if (state === states.LINE_HEIGHT && c === ' ') {
-            if (/^(\+|-)?([0-9]*\.)?[0-9]+(em|ex|ch|rem|vh|vw|vmin|vmax|px|mm|cm|in|pt|pc|%)?$/.test(buffer)) {
-                result['line-height'] = buffer;
-            }
+            result['font-family'].push(quotedStr.string);
+            i = quotedStr.endPosition - 1;
             state = states.FONT_FAMILY;
             buffer = '';
+        } else if (state === states.FONT_FAMILY && currentChar === ',') {
+            state = states.BEFORE_FONT_FAMILY;
+            buffer = '';
+        } else if (state === states.BEFORE_FONT_FAMILY && currentChar === ',') {
+            var identifier = parseIdentifier(buffer);
+            if (identifier) {
+                result['font-family'].push(identifier);
+            }
+            buffer = '';
+        } else if (state === states.VARIATION && (currentChar === ' ' || currentChar === '/')) {
+            parseVariation(buffer, result);
+            if (isFontSize(buffer)) {
+                state = currentChar === '/' ? states.LINE_HEIGHT : states.BEFORE_FONT_FAMILY;
+            }
+            buffer = '';
+        } else if (state === states.LINE_HEIGHT && currentChar === ' ') {
+            if (isLineHeight(buffer)) {
+                result['line-height'] = buffer;
+            }
+            state = states.BEFORE_FONT_FAMILY;
+            buffer = '';
         } else {
-            buffer += c;
+            buffer += currentChar;
         }
     }
 
+    // This is for the case where a string was specified followed by
+    // an identifier, but without a separating comma.
     if (state === states.FONT_FAMILY && !/^\s*$/.test(buffer)) {
-        result['font-family'].push(buffer.replace(/^\s+|\s+$/, '').replace(/\s+/g, ' '));
+        return null;
+    }
+
+    if (state === states.BEFORE_FONT_FAMILY) {
+        var fontFamily = parseIdentifier(buffer);
+
+        if (fontFamily) {
+            result['font-family'].push(fontFamily);
+        }
     }
 
     if (result['font-size'] && result['font-family'].length) {
@@ -85,29 +153,4 @@ function parse(input) {
     }
 }
 
-function quoteFamily(family) {
-    return /^(serif|sans-serif|monospace|cursive|fantasy|\$[a-zA-z]*)$/.test(family) ? family : ('"' + family + '"');
-}
-
-module.exports = function(input) {
-    if (/^(inherit|initial)$/.test(input)) {
-        return {
-            'font-size': input,
-            'line-height': input,
-            'font-style': input,
-            'font-weight': input,
-            'font-variant': input,
-            'font-stretch': input,
-            'font-family': input
-        };
-    }
-
-    input = input.replace(/\s*\/\s*/, '/');
-    let result = parse(input);
-
-    if (result) {
-        result['font-family'] = result['font-family'].map(quoteFamily).join(', ');
-    }
-
-    return result;
-};
+module.exports = parse;
--- models/componentstat/utils/getFont.js
+++ models/componentstat/utils/getFont.js
@@ -1,23 +1,36 @@
 'use strict';
 
 const fontParser = require('./cssfontparser.js');
+const quoteFamily = family =>
+    /^(serif|sans-serif|monospace|cursive|fantasy|\$[a-zA-z]*)$/.test(family) ? family : ('"' + family + '"');
 
-function getFont(value) {
-    const parse = fontParser(value);
-    const result = {
-        fontSize: '',
-        fontFamily: ''
-    };
-
-    if (parse) {
-        result.fontSize = parse['font-size'];
-        result.fontFamily = parse['font-family'];
+function prepareFontData(input) {
+    if (/^(inherit|initial)$/.test(input)) {
+        return {
+            'font-size': input,
+            'line-height': input,
+            'font-style': input,
+            'font-weight': input,
+            'font-variant': input,
+            'font-stretch': input,
+            'font-family': input
+        };
     }
 
-    return {
-        fontSize: result.fontSize,
-        fontFamily: result.fontFamily
-    };
+    input = input.replace(/\s*\/\s*/, '/');
+    let result = fontParser(input);
+
+    if (result) {
+        result['font-family'] = result['font-family'].map(quoteFamily).join(', ');
+    }
+
+    return result;
 }
 
-module.exports = getFont;
+module.exports = function (value) {
+    const parseFontString = prepareFontData(value);
+    return {
+        fontSize: parseFontString['font-size'] || '',
+        fontFamily: parseFontString['font-family'] || ''
+    };
+};
