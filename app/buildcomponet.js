8886cdbf3d7e14d6f33c401f1358f7ef1ce06d38
Dmytro Nechepurenko <dimanechepurenko@gmail.com>
2019-04-08 23:49:21 +0300

    UP: buildcomponent refactoring

--- app/buildcomponet.js
+++ app/buildcomponet.js
@@ -1,77 +1,23 @@
 'use strict';
 
-const path = require('path');
+let cachedContent = {};
+const isContentChanged = (url, content) => {
+    if (url === undefined) {
+        return true;
+    }
+    if (content !== cachedContent[url]) {
+        cachedContent[url] = content;
+        return true;
+    } else {
+        return false;
+    }
+};
 
 module.exports = function(atlasConfig, projectTree, projectImportsGraph, projectImports, writePage) {
-    const projectConstants = require(path.resolve(__dirname, '../models/projectconstants.js'))(atlasConfig.constants,
-        atlasConfig.scssAdditionalImportsArray, atlasConfig.constants.constantsFile);
-    const componentImports = src => projectImports.getFileImports(src, projectImportsGraph);
-    const componentStat = require(path.resolve(__dirname, '../models/componentstat.js'));
-    const renderedPageContent = require(path.resolve(__dirname, '../models/pagecontent.js'));
-
-    // View models
-    const statistics = require(path.resolve(__dirname, '../viewmodels/statcomponent.js'));
-    const coverage = require(path.resolve(__dirname, '../viewmodels/coverage.js'));
-    const styleguide = require(path.resolve(__dirname, '../viewmodels/styleguide.js'));
-
     // Utils
     const normalizePath = require('./utils/normalizepath');
-
-    // Prepare content model depending on component type
-    function prepareContentModel(component) {
-        let content;
-        let tableOfContent;
-        let stat;
-        let page;
-        let isNeedStat;
-
-        if (component.src !== '') { // could be stat pages or custom defined file
-            page = renderedPageContent(component.src, { 'title': component.title });
-            content = page.content;
-            tableOfContent = page.toc;
-            isNeedStat = page.isNeedStat;
-        }
-        switch (component.type) {
-            case 'styleguide':
-                content = styleguide(projectConstants);
-                break;
-            case 'component':
-            case 'container':
-                if (isNeedStat) {
-                    stat = statistics(
-                        componentStat.getStatFor(component.src, atlasConfig.componentPrefixes),
-                        componentImports(component.src),
-                        projectConstants
-                    );
-                }
-                break;
-            case 'about':
-                stat = {
-                    'projectName': atlasConfig.projectInfo.name,
-                    'coverage': coverage(projectTree.coverage)
-                };
-                break;
-        }
-
-        return {
-            documentation: content,
-            toc: tableOfContent,
-            componentStats: stat
-        };
-    }
-
-    let cachedContent = {};
-    const isContentChanged = (url, content) => {
-        if (url === undefined) {
-            return true;
-        }
-        if (content !== cachedContent[url]) {
-            cachedContent[url] = content;
-            return true;
-        } else {
-            return false;
-        }
-    };
+    const prepareContent = require('./utils/preparecontent')(
+        atlasConfig, projectTree, projectImportsGraph, projectImports).prepareContent;
 
     /**
      * Walk though documented files in project and generate particular page (if path specified)
@@ -91,7 +37,7 @@ module.exports = function(atlasConfig, projectTree, projectImportsGraph, project
                 const isFile = component.target;
 
                 if (isFile && isFileInConfig) {
-                    const content = prepareContentModel(component);
+                    const content = prepareContent(component);
                     if (isContentChanged(sourcePath, content.documentation)) {
                         docSet.push({
                             id: component.id,
